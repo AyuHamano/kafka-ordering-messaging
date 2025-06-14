dfe0ab0ddd17061646d8eab4d37cce814b17bdc9
Ayumi Hamano <98706488+AyuHamano@users.noreply.github.com>
2025-06-14 12:40:46 -0300

    chore: springboot project initialization

---
+++ src/main/java/com/ayuhamano/kafka_ordering_messaging/KafkaOrderingMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.ayuhamano.kafka_ordering_messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOrderingMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaOrderingMessagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayuhamano/kafka_ordering_messaging/KafkaOrderingMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayuhamano.kafka_ordering_messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaOrderingMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
